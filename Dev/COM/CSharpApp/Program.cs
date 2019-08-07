6e5acb898b20d3e1a181625af995a81e7ef1005c
Xuan Li <xuanli@Xuans-MacBook-Pro.local>
2019-08-07 10:22:28 +0800

    add COM

---
+++ Dev/COM/CSharpApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create the MATLAB instance 
+            MLApp.MLApp matlab = new MLApp.MLApp();
+
+            // Change to the directory where the function is located 
+            matlab.Execute(@"cd C:\Users\Xuan\Desktop\");
+
+            // Define the output 
+            object result = null;
+
+            // Call the MATLAB function myfunc
+            matlab.Feval("code_with_outputvideo", 0, out result);
+
+            // Display result 
+            object[] res = result as object[];
+
+            Console.WriteLine(res[0]);
+            Console.WriteLine(res[1]);
+            Console.ReadLine();
+        }
+    }
+}
